ba3f74d2c6e6e70b35e634047ecde861be999070
Jason Lu <jasonlu2907@gmail.com>
2020-10-11 13:56:21 -0500

    Update using PATCH + Delete document, read Mongoose document is a +

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -68,7 +68,7 @@ exports.getTour = async (req, res) => {
     try {
       const tour = await Tour.findById(req.params.id);
       // shorthand of Tour.findOne({ _id: req.params.id })
-      
+
       res.status(200).json({
         status: 'success',
         data: {
@@ -100,7 +100,7 @@ exports.createTour = async (req, res) => {
     res.status(404).json({
       status: 'fail',
       message: err
-    })
+    });
   }
 
     // USE MIDDLEWARE TO ACCESS TO THE BODY OF THE REQUEST
@@ -129,18 +129,38 @@ exports.createTour = async (req, res) => {
     // res.send("Done");
 };
 
-exports.updateTour = (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: '<Updated tour..'
-        }
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      // doc Mongoose document for this option
+      new: true,
+      runValidators: true
     });
+    res.status(200).json({
+      status: 'success',
+      data: {
+          tour
+      }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
 
-exports.deleteTour = (req, res) => {
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
     res.status(204).json({
-        status: 'success',
-        data: null
+      status: 'success',
+      data: null
     });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };

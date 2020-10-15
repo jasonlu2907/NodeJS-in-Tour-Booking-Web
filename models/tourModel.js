048e8f233b21b629b4b31890a5c18012503184f5
Jason Lu <jasonlu2907@gmail.com>
2020-10-14 20:32:56 -0500

    Remodelling Tours Model

--- models/tourModel.js
+++ models/tourModel.js
@@ -3,18 +3,63 @@ const mongoose = require('mongoose');
 // Create Schema
 const tourSchema = new mongoose.Schema({
     name: {
-        type: String,
-        required: [true, 'Tour needs a name'],
-        unique: true
+      type: String,
+      required: [true, 'Tour needs a name'],
+      unique: true,
+      trim: true
+    },
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have duration']
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have duration']
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have difficulty']
     },
     rating: {
         type: Number,
         default: 4.5
     },
+    ratingQuantity: {
+      type: Number,
+      default: 4.5
+    },
+    ratingAverage: {
+      type: Number,
+      default: 0
+    },
     price: {
-        type: Number,
-        required: [true, 'Tour needs a price']
-    }
+      type: Number,
+      required: [true, 'Tour needs a price']
+    },
+    priceDiscount: {
+      type: Number,
+    },
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a description']
+    },
+    description: {
+      type: String,
+      trim: true
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image']
+    },
+    // Array of string
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now()
+    },
+    // Different start dates in 1 tour for example
+    startDates: [Date]
 });
 // Programming convention using Uppcase for Model
 const Tour = mongoose.model('Tour', tourSchema);
